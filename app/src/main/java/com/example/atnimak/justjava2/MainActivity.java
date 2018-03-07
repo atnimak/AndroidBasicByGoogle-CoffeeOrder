1972e3e18b23c672f2acade824b2fdd96c98baf2
atnimak <atnimak@gmail.com>
2018-03-07 21:42:33 +0300

    added chocolate topping checkbox

--- app/src/main/java/com/example/atnimak/justjava2/MainActivity.java
+++ app/src/main/java/com/example/atnimak/justjava2/MainActivity.java
@@ -21,18 +21,37 @@ public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
     int price = 5;
+
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        //display(quantity);
-        if(!isWhippedCream()){
-            displayPrice("Name: Antonov Maxim\n"+"Quantity: "+quantity+"\nTotal: "+quantity *price+"$\n"+"Thank you!");
-        }else {
-            displayPrice("Name: Antonov Maxim\n"+"Quantity: "+quantity+"\nToppings: whipped cream"+"\nTotal: "+quantity *price+"$\n"+"Thank you!");
+        String totalOrder ="Antonov Maxim\n";
+        totalOrder+="Quantity: "+quantity+"\n";
+        totalOrder+="Total: "+quantity*price+"\n";
+
+        String toppings = "";
+        if(isWhippedCream()){
+           toppings+="whipped cream";
         }
 
-       // displayPrice(quantity *price);
+        if(isChokolate()){
+            if(isWhippedCream()){
+                toppings+=" and chocolate";
+            }else {
+                toppings+="chocolate";
+            }
+        }
+
+        if(!isChokolate() && !isWhippedCream()){
+            toppings="no toppings";
+        }
+
+        totalOrder+="Toppings: "+toppings+"\n";
+        totalOrder+="Thank you!";
+
+        displayPrice(totalOrder);
     }
 
     /**
@@ -48,11 +67,6 @@ public class MainActivity extends AppCompatActivity {
         display(quantity);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
     private void displayPrice(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
@@ -64,7 +78,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean isWhippedCream() {
-        CheckBox checkBox = findViewById(R.id.checkBox);
+        CheckBox checkBox = findViewById(R.id.whippedCream);
+        return checkBox.isChecked();
+    }
+
+    public boolean isChokolate() {
+        CheckBox checkBox = findViewById(R.id.chocolate);
         return checkBox.isChecked();
     }
 }
