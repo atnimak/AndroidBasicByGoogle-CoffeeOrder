0e6f80e12f0efb15d007df4d0e4c7c92b46e4121
atnimak <atnimak@gmail.com>
2018-03-08 22:31:52 +0300

    added toast and e-mail intent

--- app/src/main/java/com/example/atnimak/justjava2/MainActivity.java
+++ app/src/main/java/com/example/atnimak/justjava2/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.atnimak.justjava2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -25,36 +29,43 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int priceWithToppings = price;
-        String totalOrder = "Name: " + configName() + "\n";
-        totalOrder += "Quantity: " + quantity + "\n";
+        if(configName().equals("")){
+            Toast.makeText(getApplicationContext(), "You have to write a name", Toast.LENGTH_SHORT).show();
+        }else {
+            int priceWithToppings = price;
+            String name = configName();
+            String totalOrder = "Name: " + name + "\n";
+            totalOrder += "Quantity: " + quantity + "\n";
 
-
-        String toppings = "";
-        if (isWhippedCream()) {
-            toppings += "whipped cream";
-            priceWithToppings++;
-        }
-
-        if (isChokolate()) {
+            String toppings = "";
             if (isWhippedCream()) {
-                toppings += " and chocolate";
-                priceWithToppings += 2;
-            } else {
-                toppings += "chocolate";
-                priceWithToppings += 2;
+                toppings += "whipped cream";
+                priceWithToppings++;
             }
+
+            if (isChokolate()) {
+                if (isWhippedCream()) {
+                    toppings += " and chocolate";
+                    priceWithToppings += 2;
+                } else {
+                    toppings += "chocolate";
+                    priceWithToppings += 2;
+                }
+            }
+
+            if (!isChokolate() && !isWhippedCream()) {
+                toppings = "no toppings";
+            }
+
+            totalOrder += "Total: " + quantity * priceWithToppings + "\n";
+            totalOrder += "Toppings: " + toppings + "\n";
+            totalOrder += "Thank you!";
+
+            String[] adresses = {"order@givemecoffenow.com"};
+            composeEmail(adresses, "Just coffee order for " + name, totalOrder);
         }
 
-        if (!isChokolate() && !isWhippedCream()) {
-            toppings = "no toppings";
-        }
-
-        totalOrder += "Total: " + quantity * priceWithToppings + "\n";
-        totalOrder += "Toppings: " + toppings + "\n";
-        totalOrder += "Thank you!";
-
-        displayPrice(totalOrder);
+        // displayPrice(totalOrder);
     }
 
     /**
@@ -66,7 +77,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void decrementCoffee(View view) {
-        if (quantity > 1) quantity--;
+        if (quantity > 1) {
+            quantity--;
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "You can`t have less than one cup", Toast.LENGTH_SHORT);
+            toast.show();
+        }
         display(quantity);
     }
 
@@ -76,7 +92,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void incrementCoffee(View view) {
-        if (quantity <100 ) quantity++;
+        if (quantity < 100) {
+            quantity++;
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), "You can`t have more than 100 cups", Toast.LENGTH_SHORT);
+            toast.show();
+        }
         display(quantity);
     }
 
@@ -93,9 +114,17 @@ public class MainActivity extends AppCompatActivity {
     public String configName() {
         EditText editText = (EditText) findViewById(R.id.editName);
         String name = editText.getText().toString();
-        if (name.equals("")) {
-            name = "Заказчик";
-        }
         return name;
+    }
+
+    public void composeEmail(String[] addresses, String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
